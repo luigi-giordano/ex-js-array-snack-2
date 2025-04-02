9cfa8d6e1de6ccb2b378f2308e1054aa553d5d14
luigi-giordano <luigi.giordano1992@gmail.com>
2025-04-02 13:04:17 +0200

    [snack 1 + snack 2]

--- script.js
+++ script.js
@@ -44,3 +44,29 @@ const books = [
     tags: ['html', 'advanced', 'junior', 'mid-senior']
   },
 ];
+
+// SNACK 1
+const longBooks = books.filter(book => {
+  return book.pages > 300;
+})
+console.log(longBooks);
+
+longBooksTitles = longBooks.map(longBook => {
+  return longBook.title
+})
+console.log(longBooksTitles);
+
+longBooksTitles.forEach(title => {
+  return console.log(title);
+});
+
+// SNACK 2
+const availableBooks = books.slice(book => {
+  return book;
+})
+console.log(availableBooks);
+
+const discountedBooks = availableBooks.concat(availableBook => {
+  // return availableBook.price(Math.round((availableBook.price * 0.8) * 100) / 100)
+})
+console.log(discountedBooks);
